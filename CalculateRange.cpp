bdb65185216f51b7e619c767120d5828034367df
Zion3 <82191466+Zion3@users.noreply.github.com>
2021-09-14 21:10:09 -0500

    Update CalculateRange.cpp

--- CalculateRange.cpp
+++ CalculateRange.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	int placeHolder{}; // Variable used to ask for input at the end of the main function to stop program from instantly closing.
 
-	std::cout << "How many bits is your variable?: ";
+	std::cout << "What is the size of your integer?: ";
 	int bitAmount{};
 	std::cin >> bitAmount;
 		
