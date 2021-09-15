b93fb41cef2cf17485fb4a4568c3c377b16eac24
Zion3 <82191466+Zion3@users.noreply.github.com>
2021-09-14 21:05:42 -0500

    Add files via upload

---
+++ CalculateRange.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+	int placeHolder{}; // Variable used to ask for input at the end of the main function to stop program from instantly closing.
+
+	std::cout << "How many bits is your variable?: ";
+	int bitAmount{};
+	std::cin >> bitAmount;
+		
+	std::cout << "\nYour " << bitAmount << "-bit signed variable has a range of " 
+		<< -1 * (pow(2, bitAmount - 1)) << " to " << pow(2, bitAmount - 1) - 1 << ".\n\n";
+
+	std::cin >> placeHolder; // Stops program from instantly closing by waiting for an input.
+}
